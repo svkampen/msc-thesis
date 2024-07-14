bcbcf341c20fabd02af1a424d9a3fe581eb1f6e7
Sam van Kampen <sam@tehsvk.net>
2024-07-15 00:55:50 +0200

    Thesis.lean: add modules
    Adding the modules makes `lake build` check all files.

--- Thesis.lean
+++ Thesis.lean
@@ -1,2 +1,9 @@
 -- This module serves as the root of the `Thesis` library.
 -- Import modules here that should be built as part of the library.
+import Thesis.ARS
+import Thesis.Cofinality
+import Thesis.Multiset
+import Thesis.Newman
+import Thesis.ReductionSeq
+import Thesis.RelProps
+import Thesis.SymmSeq
